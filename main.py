752444f99f3d22cd4e93f1af7f5550369636f22f
phuong2k4 <phuongsepfg@gmail.com>
2024-07-25 18:40:36 +0200

    send message use api twilio

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from twilio.rest import Client
+SID_Key = os.environ.get("SID")
+AUTH_Key = os.environ.get("AUTH")
+UrPhone = os.environ.get("MPhone")
+TwilioPhone = os.environ.get("TPhone")
+
+client = Client(SID_Key, AUTH_Key)
+message = client.messages.create(
+    body="Send Message Complete",
+    from_= TwilioPhone,
+    to= UrPhone
+)
+print(message.status)
